0b714d842102fa2529385a2f75f7aa9c8549c421
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-27 23:52:19 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -12,39 +12,143 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Term
+{
+    int coeff;
+    int power;
+    struct Term *next;
+} Term;
+
+/**
+ * @brief Fungsi untuk membuat suku baru
+ *
+ * @param coeff coeffisien suku
+ * @param power pangkat suku
+ * @return Term*
+ */
+Term *makeNewTerm(int coeff, int power)
+{
+    Term *retval = malloc(sizeof(Term));
+    retval->coeff = coeff;
+    retval->power = power;
+    retval->next = NULL;
+    return retval;
+}
+
+/**
+ * @brief prosedur untuk memnambahkan suku baru ke polinomial
+ *
+ * @param headPtr pointer ke head dari polinomial
+ * @param newTerm suku baru yang akan dimasukkan
+ */
+void insert(Term **headPtr, Term *newTerm)
+{
+    Term *head = *headPtr;
+    if (head == NULL)
+    {
+        *headPtr = newTerm;
+    }
+    else if (head->power <= newTerm->power)
+    {
+        if (head->power == newTerm->power)
+        {
+            head->coeff += newTerm->coeff;
+            free(newTerm);
+        }
+        else
+        {
+            newTerm->next = head;
+            *headPtr = newTerm;
+        }
+    }
+    else
+    {
+        while (head->next != NULL && head->next->power > newTerm->power)
+        {
+            head = head->next;
+        }
+
+        if (head != NULL)
+        {
+            if (head->next != NULL)
+            {
+                if (head->next->power == newTerm->power)
+                {
+                    head->next->coeff += newTerm->coeff;
+                    free(newTerm);
+                }
+                else
+                {
+                    Term *temp = head;
+                    newTerm->next = head->next;
+                    head->next = newTerm;
+                }
+            }
+            else
+            {
+                head->next = newTerm;
+            }
+        }
+    }
+}
+
+/**
+ * @brief prosedur untuk mencetak polinomial
+ *
+ * @param head head dari polinomial yang ingin dicetak
+ */
+void print(Term *head)
+{
+    if (head != NULL)
+    {
+        printf("%dx^%d", head->coeff, head->power);
+        if (head->next != NULL)
+            printf(" + ");
+        print(head->next);
+    }
+}
+
 int main()
 {
-	int coeff, power;
-	char *token, buffer[256];
+    int coeff, power;
+    char *token, buffer[256];
+    Term *A = NULL;
+    Term *B = NULL;
 
-	// Masukkan polinomial A
-	printf("Masukkan Polinomial A: ");
-	scanf(" %[^\n]s", buffer);
-	token = strtok(buffer, " + ");
-	while (token != NULL)
-	{
-		sscanf(token, "%dx^%d", &coeff, &power);
+    // Masukkan polinomial A
+    printf("Masukkan Polinomial A: ");
+    scanf(" %[^\n]s", buffer);
+    token = strtok(buffer, " + ");
+    while (token != NULL)
+    {
+        sscanf(token, "%dx^%d", &coeff, &power);
+        insert(&A, makeNewTerm(coeff, power));
+        token = strtok(NULL, " + ");
+    }
 
-		// ...
+    // Masukkan polinomial B
+    printf("Masukkan Polinomial B: ");
+    scanf(" %[^\n]s", buffer);
+    token = strtok(buffer, " + ");
+    while (token != NULL)
+    {
+        sscanf(token, "%dx^%d", &coeff, &power);
+        insert(&B, makeNewTerm(coeff, power));
+        token = strtok(NULL, " + ");
+    }
 
-		token = strtok(NULL, " + ");
-	}
+    // Penjumlahan A dan B
+    while (A != NULL)
+    {
+        Term *temp = A;
+        A = A->next;
+        temp->next = NULL;
+        insert(&B, temp);
+    }
 
-	// Masukkan polinomial B
-	printf("Masukkan Polinomial B: ");
-	scanf(" %[^\n]s", buffer);
-	token = strtok(buffer, " + ");
-	while (token != NULL)
-	{
-		sscanf(token, "%dx^%d", &coeff, &power);
+    // Mencetak Hasil
+    printf("Hasil: ");
+    print(B);
 
-		//...
-
-		token = strtok(NULL, " + ");
-	}
-
-	// Mencetak Hasil
-	printf("Hasil: ");
-
-	return 0;
+    return 0;
 }
